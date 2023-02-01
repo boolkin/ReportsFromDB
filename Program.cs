d1c1d58963bf9c5aa36d256c8c2eda73e2afb7cd
boolkin <weronkz@gmail.com>
2023-02-02 02:13:16 +0300

    rebuild to list collection for pagination

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using Microsoft.Data.Sqlite;
 using System.Net;
+using System.Collections.Generic;
 
 namespace ReportDB
 {
     class Program
     {
+        static List<string[]> SQLrows = new List<string[]>();
+        static int columns;
         static void Main(string[] args)
         {
             CreateListener();
@@ -29,29 +32,35 @@ namespace ReportDB
                 HttpListenerRequest request = context.Request;
                 // Obtain a response object.
                 HttpListenerResponse response = context.Response;
-                string responseString;
+                string responseString = "";
+                // текущий каталог исполняемого файла
+                string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                var path = System.IO.Path.GetDirectoryName(strExeFilePath);
                 try
                 {
-                    string[] subs = request.RawUrl.Split(';');
-                    string htmlBlank = "<!doctype html><html lang='ru'><head><meta charset='utf-8'/><title>Отчет</title><style>td{width:20%;}</style></head><body>";
-                    responseString = htmlBlank + connectToDb(subs[1], subs[2]) + "</body></html>";
+                    string[] dates = request.RawUrl.Split(';');
+                    string reply = connectToDb(dates[1], dates[2]);
+
+                    if (reply == "ok" || reply == "full")
+                    {
+                        responseString = buildTable();
+                    }
+
+                    else {
+                        responseString = reply;
+                    }
                 }
                 catch
                 {
-                    // текущий каталог исполняемого файла
-                    string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-                    var path = System.IO.Path.GetDirectoryName(strExeFilePath);
-                    // Read the file as one string.
-                    try {
-                        string textHTML = System.IO.File.ReadAllText(System.IO.Path.Join(path, "index.html"));
-                        responseString = textHTML;
+                    try
+                    {
+                        responseString = System.IO.File.ReadAllText(System.IO.Path.Join(path, "index.html"));
                     }
-                    catch{
+                    catch
+                    {
                         responseString = "add index.html to generate page";
                     }
                 }
-
-
 
                 byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
                 // Get a response stream and write the response to it.
@@ -62,9 +71,31 @@ namespace ReportDB
 
             }
         }
+
+        static string buildTable()
+        {
+            string htmlBlank = "<!doctype html><html lang='ru'><head><meta charset='utf-8'/><style>td{width:20%;}</style></head><body><h2 align='center'>Отчет по простоям</h2>";
+            string table = "<table border='1' rules='all' align='center'><tr><th>Начало простоя</th><th>Конец простоя</th><th>Продолжительность</th></tr>";
+            string allrows = " ";
+            int i = 0;
+            while (SQLrows.Count > 0)
+            {
+                string row = "<tr>";
+                for (int j = 0; j < columns; j++)
+                {
+                    row += $"<td>{SQLrows[0][j]}</td>\n";
+                }
+                row += "</tr>";
+                allrows += row;
+                SQLrows.RemoveAt(0);
+                i++;
+                if (i == 100) break;
+            }
+            return htmlBlank + table + allrows + "</table></body></html>";
+        }
         static string connectToDb(string dateFrom, string dateTill)
         {
-            //Console.WriteLine($"start {dateFrom}, end {dateTill}");
+
             var connectionStringBuilder = new SqliteConnectionStringBuilder();
             // текущий каталог исполняемого файла
             string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
@@ -73,7 +104,7 @@ namespace ReportDB
             System.IO.Directory.CreateDirectory(folder);
             //Use DB in project directory.  If it does not exist, create it:
             connectionStringBuilder.DataSource = System.IO.Path.Join(folder, "My.db");
-
+            if (SQLrows.Count > 0) return "full";
             using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
             {
                 connection.Open();
@@ -87,19 +118,22 @@ namespace ReportDB
                     if (reader.Read())
                     {
                         var selectCmd = connection.CreateCommand();
-                        selectCmd.CommandText = $"SELECT * FROM CGL_down WHERE Downtime_start > '{dateFrom}' and Downtime_stop < date('{dateTill}','+1 day')";
+                        selectCmd.CommandText = $"SELECT * FROM CGL_down WHERE Downtime_start > '{dateFrom}' and Downtime_stop < date('{dateTill}','+1 day') ORDER BY Downtime_start ";
 
-                        string result = "<table border='1'><tr><th>Начало простоя</th><th>Конец простоя</th><th>Продолжительность</th></tr>";
                         using (var showTbl = selectCmd.ExecuteReader())
                         {
                             while (showTbl.Read())
                             {
-                                result = result + $"<tr><td>{showTbl.GetString(0)}</td><td>{showTbl.GetString(1)}</td><td>{showTbl.GetString(2)}</td></tr>";
-
+                                string[] strArr = new string[showTbl.FieldCount];
+                                for (int i = 0; i < strArr.Length; i++)
+                                {
+                                    strArr[i] = showTbl.GetString(i);
+                                }
+                                SQLrows.Add(strArr);
                             }
-                            result += "</table>";
+                            columns = showTbl.FieldCount;
                             connection.Close();
-                            return result;
+                            return "ok";
                         }
                     }
                     else
@@ -112,11 +146,11 @@ namespace ReportDB
                         using (var transaction = connection.BeginTransaction())
                         {
                             var insertCmd = connection.CreateCommand();
-                            insertCmd.CommandText = "INSERT INTO CGL_down VALUES('2023-01-28 10:00:00.000','2023-01-28 10:15:00.000','00:15:00.000')";
+                            insertCmd.CommandText = "INSERT INTO CGL_down VALUES('2023-01-31 10:00:00.000','2023-01-31 10:15:00.000','00:15:00.000')";
                             insertCmd.ExecuteNonQuery();
-                            insertCmd.CommandText = "INSERT INTO CGL_down VALUES('2023-01-29 13:00:00.000','2023-01-29 13:10:00.000','00:10:00.000')";
+                            insertCmd.CommandText = "INSERT INTO CGL_down VALUES('2023-02-01 13:00:00.000','2023-02-01 13:10:00.000','00:10:00.000')";
                             insertCmd.ExecuteNonQuery();
-                            insertCmd.CommandText = "INSERT INTO CGL_down VALUES('2023-01-30 20:30:00.000','2023-01-30 20:52:00.000','00:22:00.000')";
+                            insertCmd.CommandText = "INSERT INTO CGL_down VALUES('2023-02-01 20:30:00.000','2023-02-01 20:52:00.000','00:22:00.000')";
                             insertCmd.ExecuteNonQuery();
                             transaction.Commit();
                         }
